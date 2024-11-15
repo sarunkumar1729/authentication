2a392f0d251b92c380b9c50815c81178e10d4fd1
sarunkumar <sarunkumarvv1993@gmail.com>
2024-11-15 09:45:08 +0530

    third commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'  # URL to access media files in development
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
